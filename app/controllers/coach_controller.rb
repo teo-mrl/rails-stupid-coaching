0f81d109a0c32465a22e7eb909ab94b67e558322
Teo Morlans <teomorlans@gmail.com>
2024-02-19 15:04:13 +0100

    stupid coach

---
+++ app/controllers/coach_controller.rb
@@ -0,0 +1,13 @@
+class CoachController < ApplicationController
+  def answer
+    question = params[:question]
+    @answer = if question == "I am going to work"
+                "Great!"
+              elsif question.end_with?("?")
+                "Silly question, get dressed and go to work!"
+              else
+                "I don't care, get dressed and go to work!"
+              end
+
+end
+end
